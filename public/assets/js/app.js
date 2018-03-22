334067f1d10f45062fb4daf74a04e1632db9cefe
viviann224 <viviann224@gmail.com>
2018-03-21 22:07:44 -0500

    updated css styling

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -73,3 +73,19 @@ $(".deleteNote").on("click", function() {
         window.location = "/saved"
     })
 });
+
+//sticky header
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
